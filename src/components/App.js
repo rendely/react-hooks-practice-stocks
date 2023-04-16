8ce1eba992c5f4db11bc741fa5f14705fddef6f3
matthew rendely <matthew.rendely@gmail.com>
2023-04-16 12:58:51 -0700

    Implement fetching stock data. Implement click stock to add to portfolio but not managing state yet

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,17 @@ import React from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
+
+/* Component hierarchy
+Header
+MainContainer
+  -SearchBar
+  -StockContainer
+    -Stock[]
+  -PortfolioContainer
+    -Stock[]
+*/
+
 function App() {
   return (
     <div>
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(getStocksData, []);
+
+  function getStocksData(){
+    fetch('http://localhost:3001/stocks')
+      .then(r=>r.json())
+      .then(stocks=>setStocks(stocks))
+  }
+
+  function handleClickStock(id){
+    const stock = stocks.find(stock => stock.id === id);
+    setPortfolio([stock, ...portfolio]);
+
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} onClickStock={handleClickStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolio}/>
         </div>
       </div>
     </div>
